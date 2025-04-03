c62c4763fd34b9dc35527a7b51e71a6510987dc1
Arthur Pierron <arthur.pierron@epitech.eu>
2025-04-03 11:54:37 +0200

    [+]axx

---
+++ test.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** chocolatine
+** File description:
+** test.c
+*/
+
+#include <unistd.h>
+#include <string.h>
+
+int main(void)
+{
+    write(1, "hello\n", 6);
+    return 0;
+}
